6611ba0ed189d829f27e122e5e410544f651ce7b
brawrajab98 <brawrajab98@gmail.com>
2023-10-30 14:51:19 +0300

    setting up login + sign up screen

---
+++ lib/screens/auth.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+class AuthSreen extends StatefulWidget {
+  const AuthSreen({super.key});
+
+  @override
+  State<AuthSreen> createState() => _AuthSreenState();
+}
+
+class _AuthSreenState extends State<AuthSreen> {
+  var _isLogin = true;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(
+                  top: 30,
+                  bottom: 20,
+                  left: 20,
+                  right: 20,
+                ),
+                child: Image.asset(
+                  'assets/images/chat.png',
+                  height: 150,
+                ),
+              ),
+              Card(
+                margin: const EdgeInsets.all(20),
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Form(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          const TextField(
+                            decoration: InputDecoration(
+                              labelText: 'Email Address',
+                            ),
+                            keyboardType: TextInputType.emailAddress,
+                            autocorrect: false,
+                            textCapitalization: TextCapitalization.none,
+                          ),
+                          const TextField(
+                            decoration: InputDecoration(
+                              labelText: 'Password ',
+                            ),
+                            obscureText: true,
+                          ),
+                          const SizedBox(height: 12),
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: Theme.of(context)
+                                    .colorScheme
+                                    .primaryContainer),
+                            child: Text(_isLogin ? 'Login' : 'Sign Up'),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              setState(() {
+                                _isLogin = !_isLogin;
+                              });
+                            },
+                            child: Text(_isLogin
+                                ? 'Create New Account'
+                                : 'Already have an account? Login'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
