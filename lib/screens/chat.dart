271bba632d21bfde04c62b163ffad99e9c34a451
brawrajab98 <brawrajab98@gmail.com>
2023-10-31 11:28:03 +0300

    adding chat_messages.dart + new_messages.dart

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,3 +1,5 @@
+import 'package:chat_app/screens/widgets/chat_messages.dart';
+import 'package:chat_app/screens/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -7,23 +9,28 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Chat App'),
-        actions: [
-          IconButton(
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-            },
-            icon: Icon(
-              Icons.logout,
-              color: Theme.of(context).colorScheme.primary,
+        appBar: AppBar(
+          title: const Text('Chat App'),
+          actions: [
+            IconButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+              },
+              icon: Icon(
+                Icons.logout,
+                color: Theme.of(context).colorScheme.primary,
+              ),
             ),
-          ),
-        ],
-      ),
-      body: const Center(
-        child: Text('Logged in'),
-      ),
-    );
+          ],
+        ),
+        body: const Column(
+          children: [
+            // expanded makes 'chatMessages' take all the available space
+            Expanded(
+              child: ChatMessages(),
+            ),
+            NewMessage(),
+          ],
+        ));
   }
 }
---
+++ lib/screens/widgets/new_message.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class NewMessage extends StatefulWidget {
+  const NewMessage({super.key});
+
+  @override
+  State<NewMessage> createState() => _NewMessageState();
+}
+
+class _NewMessageState extends State<NewMessage> {
+  final _messageController = TextEditingController();
+
+// with using controllers use dispose to avoid memory leaks
+  @override
+  void dispose() {
+    _messageController.dispose();
+    super.dispose();
+  }
+
+  void _submitMessage() {
+    final enteredMessage = _messageController.text.trim();
+
+    if (enteredMessage.trim().isEmpty) {
+      return;
+    }
+
+    // sending message to firestore
+    // FirebaseFirestore.instance.collection('chat').add({
+    //   'text': enteredMessage,
+    //   'createdAt': Timestamp.now(),
+    // });
+
+    // clearing the input field
+    _messageController.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 15,
+        right: 1,
+        bottom: 14,
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: _messageController,
+              textCapitalization: TextCapitalization.sentences,
+              autocorrect: true,
+              enableSuggestions: true,
+              decoration: const InputDecoration(labelText: 'Send a message...'),
+            ),
+          ),
+          IconButton(
+            color: Theme.of(context).colorScheme.primary,
+            onPressed: _submitMessage,
+            icon: const Icon(Icons.send),
+          ),
+        ],
+      ),
+    );
+  }
+}
